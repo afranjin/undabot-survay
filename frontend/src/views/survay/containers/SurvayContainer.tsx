22d10888c43261d55d88d65749cf7a7be263cd35
Tunjo <antun.franjin@gmail.com>
2022-08-30 21:49:33 +0200

    Add create survay answers to survay component, dispach action.

--- frontend/src/views/survay/containers/SurvayContainer.tsx
+++ frontend/src/views/survay/containers/SurvayContainer.tsx
@@ -1,30 +1,39 @@
 import React, { useEffect } from 'react'
 import { Block, Box, Columns, Container } from 'react-bulma-components'
 import { connect } from 'react-redux'
+import { useNavigate } from 'react-router'
 import { RootState } from '../../../store/rootState'
-import { ISurvay } from '../../../types/survay'
+import { ISurvay, ISurvayAnswer } from '../../../types/survay'
 import * as SurvayActions from '../actions/survayActions'
 import SurvayFormComponent from '../componets/SurvayFormComponent'
 
 interface StateProps {
     survay: ISurvay
-  }
+}
   
-  interface DispatchProps {
+interface DispatchProps {
     getSurvay: () => Promise<ISurvay>
-  }
+    createSurvayAnswer: (survayId: string | number, answer: ISurvayAnswer) => Promise<ISurvayAnswer>
+}
   
-  type Props = StateProps & DispatchProps
-
+type Props = StateProps & DispatchProps
 
 const SurvayContainer = (props: Props): JSX.Element => {
-    const {survay, getSurvay} = props
-
-    //const [survay, setSurvay] = useState<ISurvay>({} as ISurvay)
+    const {survay, getSurvay, createSurvayAnswer} = props
+    const _navigate = useNavigate()
 
     useEffect(() => {
         getSurvay()
     }, [getSurvay])
+
+    const handleCreateSurvayAnswer = (answer: ISurvayAnswer): void => {
+        console.log(answer)
+        console.log(survay)
+        const _ansvers = {...answer, survay: survay}
+        createSurvayAnswer(survay.id, _ansvers).then(() => {
+            _navigate('/survay/answers')
+        })
+    }
 
     return (
         <React.Fragment>
@@ -41,7 +50,7 @@ const SurvayContainer = (props: Props): JSX.Element => {
                             <Block className='subtitle is-6 has-text-weight-light has-text-centered'>
                                 {survay.desscription_record}
                             </Block>
-                            <SurvayFormComponent survay={survay}/>
+                            <SurvayFormComponent survay={survay} createSurvayAnswer={handleCreateSurvayAnswer}/>
                         </Box>
                     </Columns.Column>
                 </Container> : null}
@@ -59,6 +68,7 @@ const mapDispatchToProps = (
 dispatch: SurvayActions.SurvayThunkDispatchType): DispatchProps => {
     return {
         getSurvay: () => dispatch(SurvayActions.getSurvay()),
+        createSurvayAnswer: (survayId, answer) => dispatch(SurvayActions.createSurvayAnswer(survayId, answer))
     }
 }
   
