7176465cd4b8a31d25ac8b58e18d413b925702cd
Tunjo <antun.franjin@gmail.com>
2022-08-30 21:38:10 +0200

    Add route path of list answers component.

--- frontend/src/views/SurvayRoutes.tsx
+++ frontend/src/views/SurvayRoutes.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import SurvayContainer from '../views/survay/containers/SurvayContainer'
+import SurvayAnswersContainer from './survay/containers/SurvayAnswersContainer'
 
 
 const SurvayRoutes = (): JSX.Element => {
   return (
     <Routes>
-      <Route path='/survay' element={<SurvayContainer/>}/>
+      <Route path='/survay' element={<SurvayContainer/>} />
+      <Route path='/survay/answers' element={<SurvayAnswersContainer/>} />
     </Routes>
   )
 }
