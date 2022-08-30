15dc09effad6d3410ef6bc9b77641d41f5ac57d1
Tunjo <antun.franjin@gmail.com>
2022-08-30 21:39:20 +0200

    Add create & get list answers actions and success methods.

--- frontend/src/views/survay/actions/survayActions.ts
+++ frontend/src/views/survay/actions/survayActions.ts
@@ -3,58 +3,113 @@ import * as constants from './constants'
 import { ActionCreator } from 'redux'
 import { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import { SurvayState } from '../reducers'
-import { ISurvay } from '../../../types/survay'
+import { ISurvay, ISurvayAnswer } from '../../../types/survay'
 
 
 export type SurvayThunkResult<R> = ThunkAction<Promise<R>, SurvayState, null, SurvayActions>
 export type SurvayThunkDispatchType = ThunkDispatch<SurvayState, null, SurvayActions>
 
 export interface SurvaySuccess {
-  type: constants.GET_SURVAY_SUCCESS;
-  survay: ISurvay;
+    type: constants.GET_SURVAY_SUCCESS
+    survay: ISurvay
+}
+
+export interface CreateAnswerSuccess {
+    type: constants.CREATE_SURVAY_ANSWER_SUCCESS
+    survayAnswer: ISurvayAnswer
+}
+
+export interface GetSurvayAnswersSuccess {
+    type: constants.GET_SURVAY_ANSWERS_SUCCESS
+    survayAnswers: ISurvayAnswer[]
 }
 
 export interface SurvayHasErrored {
-  type: constants.SURVAY_HAS_ERRORED;
-  hasErrored: boolean;
+    type: constants.SURVAY_HAS_ERRORED
+    hasErrored: boolean
 }
 
-
 export const survayHasErrored = (hasErrored: boolean, error: Error, action?: string): SurvayHasErrored => {
-  if (action) {
-    const errorMsg = error.message
-    console.log(errorMsg)
-  }
-  return {
-    type: constants.SURVAY_HAS_ERRORED,
-    hasErrored: hasErrored
-  }
+    if (action) {
+        const errorMsg = error.message
+        console.log(errorMsg)
+    }
+    return {
+        type: constants.SURVAY_HAS_ERRORED,
+        hasErrored: hasErrored
+    }
 }
 
 // thunk action, with side effect
 export const getSurvay: ActionCreator<SurvayThunkResult<ISurvay>>
-= (): SurvayThunkResult<ISurvay> => {
+= (queryParams?: Record<string, string>): SurvayThunkResult<ISurvay> => {
   return async (dispatch: SurvayThunkDispatchType) => {
-    return SurvayApi.getSurvay()
-      .then(response => {
-        dispatch(getSurvaySuccess(response))
-        return response
-      })
-      .catch(error => {
-        dispatch(survayHasErrored(true, error.response.data, constants.GET_SURVAY))
-        return {} as ISurvay
-      })
+    return SurvayApi.getSurvay(queryParams)
+        .then(response => {
+            dispatch(getSurvaySuccess(response))
+            return response
+        })
+        .catch(error => {
+            dispatch(survayHasErrored(true, error.response.data, constants.GET_SURVAY))
+            return {} as ISurvay
+        })
   }
 }
 
 // action, pure function
 export const getSurvaySuccess = (survay: ISurvay): SurvaySuccess => {
-  return {
-    type: constants.GET_SURVAY_SUCCESS,
-    survay
+    return {
+        type: constants.GET_SURVAY_SUCCESS,
+        survay
+    }
+}
+
+export const createSurvayAnswer: ActionCreator<SurvayThunkResult<ISurvayAnswer>>
+= (survayId: string | number, answer: ISurvayAnswer): SurvayThunkResult<ISurvayAnswer> => {
+  return async (dispatch: SurvayThunkDispatchType) => {
+    return SurvayApi.createAnswersSurvay(survayId, answer)
+        .then(response => {
+            dispatch(createAnswerSuccess(response))
+            return response
+        })
+        .catch(error => {
+            dispatch(survayHasErrored(true, error.response.data, constants.CREATE_SURVAY_ANSWER))
+            return {} as ISurvayAnswer
+        })
   }
 }
 
+export const createAnswerSuccess = (survayAnswer: ISurvayAnswer): CreateAnswerSuccess => {
+    return {
+        type: constants.CREATE_SURVAY_ANSWER_SUCCESS,
+        survayAnswer
+    }
+  }
+
+  export const getSurvayAnswers: ActionCreator<SurvayThunkResult<ISurvayAnswer[]>>
+= (survayId: string | number, queryParams?: Record<string, string>): SurvayThunkResult<ISurvayAnswer[]> => {
+  return async (dispatch: SurvayThunkDispatchType) => {
+    return SurvayApi.getSurvayAnswers(survayId, queryParams)
+        .then(response => {
+            dispatch(getSurvayAnswersSuccess(response))
+            return response
+        })
+        .catch(error => {
+            dispatch(survayHasErrored(true, error.response.data, constants.GET_SURVAY_ANSWERS))
+            return {} as ISurvayAnswer[]
+        })
+  }
+}
+
+export const getSurvayAnswersSuccess = (survayAnswers: ISurvayAnswer[]): GetSurvayAnswersSuccess => {
+    return {
+        type: constants.GET_SURVAY_ANSWERS_SUCCESS,
+        survayAnswers
+    }
+  }
+
 export type SurvayActions
-  = SurvayHasErrored
-  | SurvaySuccess;
+    = SurvayHasErrored
+    | SurvaySuccess
+    | CreateAnswerSuccess
+    | GetSurvayAnswersSuccess
