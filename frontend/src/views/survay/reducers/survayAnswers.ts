4cecec008f54ad406b6962a21026e340343b0aae
Tunjo <antun.franjin@gmail.com>
2022-08-30 21:51:15 +0200

    Add app answers rreducer.

---
+++ frontend/src/views/survay/reducers/survayAnswers.ts
@@ -0,0 +1,16 @@
+import { ISurvayAnswer } from "../../../types/survay";
+import * as constants from '../actions/constants'
+import { SurvayActions } from "../actions/survayActions";
+
+
+export const survayAnswers = (state: ISurvayAnswer[] = [], action: SurvayActions): ISurvayAnswer[] => {
+    switch(action.type) {
+        case constants.CREATE_SURVAY_ANSWER_SUCCESS:
+            return [...state, action.survayAnswer]
+        case constants.GET_SURVAY_ANSWERS_SUCCESS:
+            return action.survayAnswers
+    
+        default:
+            return state;
+        }
+  }
