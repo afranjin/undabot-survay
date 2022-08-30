852582d42958807214a518915c32c9c1b84fdc7e
Tunjo <antun.franjin@gmail.com>
2022-08-30 13:40:39 +0200

    Add action constants for get survay

---
+++ frontend/src/views/survay/actions/constants.ts
@@ -0,0 +1,8 @@
+export const GET_SURVAY = '[Survay] GET_SURVAY';
+export type GET_SURVAY = typeof GET_SURVAY;
+
+export const GET_SURVAY_SUCCESS = '[Survay] GET_SURVAY_SUCCESS';
+export type GET_SURVAY_SUCCESS = typeof GET_SURVAY_SUCCESS;
+
+export const SURVAY_HAS_ERRORED = '[Survay] SURVAY_HAS_ERRORED';
+export type SURVAY_HAS_ERRORED = typeof SURVAY_HAS_ERRORED;
