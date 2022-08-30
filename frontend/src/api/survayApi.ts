04852b2908b30a5b75f33bb9fd118384cf3f192b
Tunjo <antun.franjin@gmail.com>
2022-08-30 14:05:08 +0200

    Add create answwers api.

--- frontend/src/api/survayApi.ts
+++ frontend/src/api/survayApi.ts
@@ -1,9 +1,13 @@
-import { ISurvay } from '../types/survay'
+import { ISurvay, ISurvayAnswer } from '../types/survay'
 import { HttpClient } from './service'
 import Urls from './urls'
 
 export const SurvayApi = {
   getSurvay(queryParams?: Record<string, string>): Promise<ISurvay> {
     return HttpClient.get<ISurvay, Record<string, string>>(Urls.SurvayUrls.survay, queryParams)
+  },
+
+  createAnswersSurvay(answer: ISurvayAnswer): Promise<ISurvayAnswer> {
+    return HttpClient.post<ISurvayAnswer, ISurvayAnswer>(`${Urls.SurvayUrls.survay}${answer.survay.id}/answers/`, answer)
   }
 }
